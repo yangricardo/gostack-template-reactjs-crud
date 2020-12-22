a34abde7fa6b9b6d21b0025388303614824b0278
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-12-21 21:03:05 -0300

    editing food

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -51,7 +51,19 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    try {
+      const response = await api.put(`/foods/${editingFood.id}`, {
+        ...editingFood,
+        ...food,
+      });
+      setFoods(
+        foods.map(mappedFood =>
+          mappedFood.id === editingFood.id ? { ...response.data } : mappedFood,
+        ),
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
@@ -72,6 +84,7 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
+    setEditingFood(food);
     toggleEditModal();
   }
 
